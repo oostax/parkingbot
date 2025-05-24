2450b6f9d98881de475c7e4fd85a7bf7c6da1d8f
skyworker <quintinfine@gmail.com>
2025-05-24 16:36:44 +0300

    add pb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -353,7 +353,7 @@ export default function Home() {
                     </span>
                   )}
                 </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-h-[70vh] overflow-y-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-h-[70vh] overflow-y-auto pb-10">
                   {filteredParkings.length > 0 ? (
                     filteredParkings.map((parking) => (
                       <div 
